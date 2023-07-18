801ff9455d2e2c4c996693eda1fa6bc1d2a17750
Vivek Nigam <vivek.nigam@rocketium.com>
2023-07-18 22:17:22 +0530

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import rocketiumLogo from "./assets/logo.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://rocketium.com̦" target="_blank">
+          <img src={rocketiumLogo} className="logo" alt="React logo" />
+        </a>
+      </div>
+      <h2>Analytics Dashboards</h2>
+    </>
+  );
+}
+
+export default App;
